c81cdfb26efb3bc6f028f4258c080e59d536c203
Abhishek Jain <abhishek147jain@gmail.com>
2014-10-30 13:06:10 +0530

    Changing the Header Formats
    Using Helper.js and Resumebuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,23 @@
-$("#main").append("Abhishek Jain")
+
+//var awesomeThoughts
+//awesomeThoughts = "I am Abhishek Jain !";	
+//console.log(awesomeThoughts);
+
+var awesomeThoughts = "I am Abhishek Jain and I am AWESOME!";	
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+
+$("#main").append(funThoughts);
+
+var email = "abhishek@csc.com";
+var newmail = email.replace("csc","gmail");
+
+
+var formattedName = HTMLheaderName.replace("%data%","Abhishek Jain");
+var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+
+
+console.log("Old Mail ID:" + email);
+console.log("New Mail ID:" + newmail);
